73dddf8bc2df4e743a15e16b5e1543a3a5e44138
annu12340 <annujolly17@gmail.com>
2020-06-13 09:56:45 +0530

    added a section for doc

--- basic_Code/Main.py
+++ basic_Code/Main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -24,7 +22,16 @@ class Appointment(db.Model):
     time = db.Column(db.Integer)
     mode = db.Column(db.String(10))
     createdby_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    createdby_name=db.Column(db.String(50))
     status = db.Column(db.String(50), default='Pending')
+
+
+# *****  DOCTOR SIDE *****
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+
 
 ################################  REGISTER  LOGIN  LOGOUT ROUTES ###################################
 
@@ -80,6 +87,8 @@ def showAppointment():
     return render_template('showAppointment.html', show_appointment=show_appointment)
 
 
+
+
 ####################################  OTHER ROUTES  #########################################
 
 @app.route('/index')
@@ -90,7 +99,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/appointment',methods=['GET', 'POST'])
+@app.route('/appointment', methods=['GET', 'POST'])
 def appointment():
     if request.method == 'POST':
         user_id = session['user']
@@ -98,7 +107,8 @@ def appointment():
         new_appointment = Appointment(docName=request.form['docName'],
                                       department=request.form['department'],
                                       date=request.form['date'], time=request.form['time'], mode=request.form['mode'],
-                                      createdby_id=user_id)
+                                      createdby_id=user_id, createdby_name=User.query.get(user_id).username
+)
 
         db.session.add(new_appointment)
         db.session.commit()
@@ -106,6 +116,60 @@ def appointment():
     else:
         return render_template('appointment.html')
 
+
+######################################### ROUTES FOR THE DOCTORS ####################################
+
+# *****  DOCTOR SIDE *****
+
+@app.route('/dlogin', methods=['GET', 'POST'])
+def dlogin():
+    if request.method == 'GET':
+        return render_template('dlogin.html')
+    else:
+        username = request.form['username']
+        password = hash(request.form['password'])
+        data = Doctor.query.filter_by(username=username,
+                                    password=password).first()
+
+        if data is not None:
+            session['doctor'] = data.id
+            print(session['doctor'])
+            return redirect(url_for('dindex'))
+        return render_template('incorrectLogin.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/dregister/', methods=['GET', 'POST'])
+def dregister():
+    if request.method == 'POST':
+        new_user = Doctor(username=request.form['username'],
+                        password=hash(request.form['password']))
+
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('dlogin'))
+    return render_template('dregister.html')
+
+
+@app.route('/dlogout', methods=['GET', 'POST'])
+def dlogout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+
+
+@app.route('/dindex')
+def dindex():
+    return render_template('dindex.html')
+
+
+@app.route('/viewAppointments')
+def viewAppointments():
+    doctor_id = session['doctor']
+    docname = Doctor.query.get(doctor_id).username
+    myAppointments = Appointment.query.filter_by(docName=docname).all()
+    print(myAppointments)
+    return render_template('viewAppointments.html', myAppointments=myAppointments)
 
 ######################################### MAIN ####################################
 
