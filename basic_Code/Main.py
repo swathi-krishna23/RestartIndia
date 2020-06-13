dbeffea6f44ffa5c72850fd9f0079d9ab379d025
annu12340 <annujolly17@gmail.com>
2020-06-13 09:13:59 +0530

    added the appointment section (in user side)

--- basic_Code/Main.py
+++ basic_Code/Main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,7 +16,15 @@ class User(db.Model):
     password = db.Column(db.String(50))
 
 
-
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    docName = db.Column(db.String(50))
+    department = db.Column(db.String(50))
+    date = db.Column(db.Integer)
+    time = db.Column(db.Integer)
+    mode = db.Column(db.String(10))
+    createdby_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.String(50), default='Pending')
 
 ################################  REGISTER  LOGIN  LOGOUT ROUTES ###################################
 
@@ -36,6 +45,7 @@ def login():
             return redirect(url_for('index'))
 
         return render_template('incorrectLogin.html')
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/register/', methods=['GET', 'POST'])
@@ -64,6 +74,10 @@ def show():
     return render_template('show.html', show_user=show_user)
 
 
+@app.route('/showAppointment')
+def showAppointment():
+    show_appointment = Appointment.query.all()
+    return render_template('showAppointment.html', show_appointment=show_appointment)
 
 
 ####################################  OTHER ROUTES  #########################################
@@ -76,6 +90,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/appointment',methods=['GET', 'POST'])
+def appointment():
+    if request.method == 'POST':
+        user_id = session['user']
+        print(user_id)
+        new_appointment = Appointment(docName=request.form['docName'],
+                                      department=request.form['department'],
+                                      date=request.form['date'], time=request.form['time'], mode=request.form['mode'],
+                                      createdby_id=user_id)
+
+        db.session.add(new_appointment)
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return render_template('appointment.html')
 
 
 ######################################### MAIN ####################################
